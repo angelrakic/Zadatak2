92a6cdb873a0bc49604792b48098464480296e4a
angelrakic <andjela.rakic@orioninc.com>
2021-09-30 13:09:27 +0200

    Initial Commit

---
+++ noviZadatak/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  noviZadatak
+//
+//  Created by Andjela Rakic on 9/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
